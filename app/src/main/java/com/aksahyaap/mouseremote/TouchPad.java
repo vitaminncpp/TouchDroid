86949cc718bb884e9f32fd2cd36964c4c7b42ce3
Akshayaap <akshaykumarparmar51@gmail.com>
2022-01-28 17:09:49 +0530

    Everything Works Fine

--- app/src/main/java/com/aksahyaap/mouseremote/TouchPad.java
+++ app/src/main/java/com/aksahyaap/mouseremote/TouchPad.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.annotation.SuppressLint;
+import android.app.ActionBar;
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.io.DataOutputStream;
@@ -16,12 +19,12 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 public class TouchPad extends AppCompatActivity {
-    private int X=0;
-    private int Y=0;
-    private int Xp=0;
-    private int Yp=0;
-    private int dx=0;
-    private int dy=0;
+    private int X = 0;
+    private int Y = 0;
+    private int Xp = 0;
+    private int Yp = 0;
+    private int dx = 0;
+    private int dy = 0;
     private Sender sender;
     private Socket client;
     private PrintWriter pw;
@@ -34,48 +37,49 @@ public class TouchPad extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_touch_pad);
-        TextView txtIP=findViewById(R.id.txtIP);
-        TextView txtPort=findViewById(R.id.txtPort);
+        data = new MData();
 
-        data=new MData();
+        String ip = getIntent().getStringExtra("ip");
+        String port = getIntent().getStringExtra("port");
+        Log.i("IP and Port", ip + "   " + port);
 
-        String ip=getIntent().getStringExtra("ip");
-        String port=getIntent().getStringExtra("port");
-        Log.i("IP and Port",ip+"   "+port);
-        txtIP.setText(ip);
-        txtPort.setText(port);
-        ProgressDialog dialog=new ProgressDialog(this);
+        ProgressDialog dialog = new ProgressDialog(this);
 
-        ConstraintLayout layout =(ConstraintLayout)findViewById(R.id.layout_touchpad);
+        ConstraintLayout layout = (ConstraintLayout) findViewById(R.id.touchpad);
+        ConstraintLayout wheel = (ConstraintLayout) findViewById(R.id.wheel);
+        Button btnLeft = findViewById(R.id.btnLeft);
+        Button btnRight = findViewById(R.id.btnRight);
+
         dialog.show();
 
-        Log.d("!!!connecting to: ",ip+":"+port);
-        new Thread(new clientThread(ip,port)).start();
+        Log.d("!!!connecting to: ", ip + ":" + port);
+        new Thread(new clientThread(ip, port)).start();
 
         dialog.dismiss();
 
         layout.setOnTouchListener((view, motionEvent) -> {
-            int eventType= motionEvent.getActionMasked();
-            switch (eventType){
+            int eventType = motionEvent.getActionMasked();
+            switch (eventType) {
+
                 case MotionEvent.ACTION_UP:
-                    Xp=0;
-                    Yp=0;
+                    Xp = 0;
+                    Yp = 0;
                     break;
                 case MotionEvent.ACTION_MOVE:
-                    X= (int) motionEvent.getX();
-                    Y= (int) motionEvent.getY();
-                    dx=X-Xp;
-                    dy=Y-Yp;
-                    data.X=X;
-                    data.Y=Y;
-                    data.dx=dx;
-                    data.dy=dy;
-                    if(Xp!=0&&Yp!=0)
-                        new Thread(new senderThread(String.format("%6d",dx)+"    "+String.format("%6d",dy)+"    end")).start();
+                    X = (int) motionEvent.getX();
+                    Y = (int) motionEvent.getY();
+                    dx = X - Xp;
+                    dy = Y - Yp;
+                    data.X = X;
+                    data.Y = Y;
+                    data.dx = dx;
+                    data.dy = dy;
+                    if (Xp != 0 && Yp != 0)
+                        new Thread(new senderThread(String.format("%6d    %6d    M      end",dx,dy))).start();
 
-                    Xp=X;
-                    Yp=Y;
-                    Log.d("!!!",data.X+" "+data.Y+" "+data.dx+" "+data.dy);
+                    Xp = X;
+                    Yp = Y;
+                    Log.d("!!!", data.X + " " + data.Y + " " + data.dx + " " + data.dy);
 
                     break;
                 default:
@@ -83,18 +87,83 @@ public class TouchPad extends AppCompatActivity {
             }
             return true;
         });
+
+
+        btnLeft.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+
+                int type=motionEvent.getActionMasked();
+                switch(type){
+                    case MotionEvent.ACTION_DOWN:
+                        new Thread(new senderThread("     0         0    L          end")).start();
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        new Thread(new senderThread("     0         0    l      end")).start();
+                        break;
+                }
+
+                return true;
+            }
+        });
+
+
+
+
+        btnRight.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                int type=motionEvent.getActionMasked();
+                switch(type){
+                    case MotionEvent.ACTION_DOWN:
+                        new Thread(new senderThread("     0         0    R      end")).start();
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        new Thread(new senderThread("     0         0    r      end")).start();
+                        break;
+                }
+                return true;
+            }
+        });
+
+        wheel.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                int type = motionEvent.getActionMasked();
+
+                switch (type) {
+                    case MotionEvent.ACTION_UP:
+                        Yp = 0;
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        Y = (int) motionEvent.getY();
+                        dy = Y - Yp;
+                        if (Yp != 0)
+                            new Thread(new senderThread(String.format("%6d         0    W      end", dy))).start();
+                        Yp = Y;
+                        break;
+                    default:
+                        break;
+                }
+                return true;
+            }
+        });
     }
 
-    class clientThread implements Runnable{
+
+    class clientThread implements Runnable {
         String ip;
         String port;
-        clientThread(String ip, String port){
+
+        clientThread(String ip, String port) {
             this.ip = ip;
             this.port = port;
         }
+
         public void run() {
             try {
-                Log.d("!!!connecting to: ",ip+":"+port);
+                Log.d("!!!connecting to: ", ip + ":" + port);
                 client = new Socket(this.ip, Integer.parseInt(this.port));
                 dos = new DataOutputStream(client.getOutputStream());
 
@@ -105,14 +174,17 @@ public class TouchPad extends AppCompatActivity {
         }
     }
 
-    class senderThread implements Runnable{
+    class senderThread implements Runnable {
         String str;
-        senderThread(String str){
+
+        senderThread(String str) {
             this.str = str;
         }
+
         public void run() {
             try {
-                dos.writeBytes(str + "\n");
+                dos.writeBytes(str );
+                Log.i("String", String.valueOf(str.length()));
             } catch (IOException e) {
                 e.printStackTrace();
             }

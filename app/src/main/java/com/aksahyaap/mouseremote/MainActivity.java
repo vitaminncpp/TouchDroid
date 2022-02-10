c6e6ae1742b36f1f6feea512a804fb8761ba2837
SKR301 <chunnu.bihat@gmail.com>
2022-02-10 21:57:44 +0530

    #10 updated Colours

--- app/src/main/java/com/aksahyaap/mouseremote/MainActivity.java
+++ app/src/main/java/com/aksahyaap/mouseremote/MainActivity.java
@@ -13,24 +13,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        EditText mainTxtIP = findViewById(R.id.mainTxtIP);
-        EditText mainTxtPort = findViewById(R.id.mainNumPort);
 
+        
 
-        Button button = findViewById(R.id.btnConnect);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String ip = mainTxtIP.getText().toString().trim();
-                String port = mainTxtPort.getText().toString().trim();
-                Intent intent = new Intent(MainActivity.this, TouchPad.class);
-                intent.putExtra("ip", ip);
-                intent.putExtra("port", port);
-
-                startActivity(intent);
-            }
-        });
     }
 }

43cc57c5700050cd0decf4e9e5e01fbbe7e7143a
SKR301 <chunnu.bihat@gmail.com>
2022-02-10 22:09:32 +0530

    #10 added wifi toggle button

--- app/src/main/java/com/aksahyaap/mouseremote/MainActivity.java
+++ app/src/main/java/com/aksahyaap/mouseremote/MainActivity.java
@@ -13,8 +13,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        
+
 
     }
 }

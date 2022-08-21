1a5bc638d64a110891b7bbcff745ae49c848cf2f
Akshayaap <Akshaykumarparmar51@gmail.com>
2022-08-22 00:21:58 +0530

    Keyboard

---
+++ app/src/main/java/com/akshayaap/mouseremote/KeyBoard.java
@@ -0,0 +1,14 @@
+package com.akshayaap.mouseremote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KeyBoard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_key_board);
+    }
+}

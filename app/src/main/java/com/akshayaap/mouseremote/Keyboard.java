8f4819df8066ad269dceb99fa8b2eef1c1a8bcbf
Akshayaap <Akshaykumarparmar51@gmail.com>
2022-08-23 14:33:38 +0530

    UI is changed

--- app/src/main/java/com/akshayaap/mouseremote/Keyboard.java
+++ app/src/main/java/com/akshayaap/mouseremote/Keyboard.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class KeyBoard extends AppCompatActivity {
+public class Keyboard extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_key_board);
+        setContentView(R.layout.activity_keyboard);
     }
 }

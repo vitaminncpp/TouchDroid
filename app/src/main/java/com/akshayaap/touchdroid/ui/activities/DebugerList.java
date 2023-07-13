1bd570e472fe803eef604d2f5f675fc21d0dfcf1
Akshayaap <Akshaykumarparmar51@gmail.com>
2023-07-14 02:14:49 +0530

    Added new DebugList Activity

---
+++ app/src/main/java/com/akshayaap/touchdroid/ui/activities/DebugerList.java
@@ -0,0 +1,16 @@
+package com.akshayaap.touchdroid.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.akshayaap.touchdroid.R;
+
+public class DebugerList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_debuger_list);
+    }
+}

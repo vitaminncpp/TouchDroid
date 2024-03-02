16b57d39ac035c1a1ea3e825602593f82f606c0a
Akshayaap <Akshaykumarparmar51@gmail.com>
2024-03-02 15:09:12 +0530

    gradle upgrade

--- app/src/main/java/com/akshayaap/touchdroid/ui/activities/Debugger.java
+++ app/src/main/java/com/akshayaap/touchdroid/ui/activities/Debugger.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.akshayaap.touchdroid.R;
 
-public class DebugerList extends AppCompatActivity {
+public class Debugger extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_debuger_list);
+        setContentView(R.layout.activity_debuger);
     }
 }

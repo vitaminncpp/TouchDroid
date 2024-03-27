730b8f77042b3e554f3eb03d6497f88cadd7fcb3
Akshayaap <Akshaykumarparmar51@gmail.com>
2024-03-28 00:15:35 +0530

    Added wifi logo animation

--- app/src/main/java/com/akshayaap/touchdroid/ui/fragments/Network.java
+++ app/src/main/java/com/akshayaap/touchdroid/ui/fragments/Network.java
@@ -3,14 +3,30 @@ package com.akshayaap.touchdroid.ui.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.akshayaap.touchdroid.R;
+import com.akshayaap.touchdroid.ui.activities.MainActivity;
+import com.akshayaap.touchdroid.ui.activities.MasterActivity;
+import com.akshayaap.touchdroid.ui.adapters.WifiListAdapter;
+import com.akshayaap.touchdroid.util.Server;
+
+import java.util.ArrayList;
 
 public class Network extends Fragment {
+    View view;
+    ImageView imageView_wifiLogo;
+    RecyclerView recyclerView_serverList;
+    WifiListAdapter adapter;
+    ArrayList<Server> tempWifiList;
+    MainActivity.Connecting conn;
 
     public Network() {
 
@@ -24,6 +40,12 @@ public class Network extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_network, container, false);
+        view = inflater.inflate(R.layout.fragment_network, container, false);
+        imageView_wifiLogo = view.findViewById(R.id.imageView_wifiLogo);
+        Animation animation = AnimationUtils.loadAnimation(getContext(), R.anim.fade_in_fade_out);
+        imageView_wifiLogo.startAnimation(animation);
+
+        return view;
+
     }
 }
